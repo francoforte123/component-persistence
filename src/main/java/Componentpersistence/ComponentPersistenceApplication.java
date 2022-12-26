df4e1b3fc83ef6a9152740fe29151ef817783df0
Emmanuele La Duca <laducaemanuele20@gmail.com>
2022-12-26 23:16:19 +0100

    add functionality

---
+++ src/main/java/Componentpersistence/ComponentPersistenceApplication.java
@@ -0,0 +1,13 @@
+package Componentpersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentPersistenceApplication.class, args);
+	}
+
+}
